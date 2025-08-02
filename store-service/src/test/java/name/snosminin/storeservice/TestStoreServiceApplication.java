d5dca5a470a32c96c51c032b7eb72edb81887567
Sergei <>
2025-08-02 21:12:07 +0600

    Remove unused file

--- store-service/src/test/java/name/snosminin/storeservice/TestStoreServiceApplication.java
+++ store-service/src/test/java/name/snosminin/storeservice/TestStoreServiceApplication.java
@@ -5,6 +5,6 @@ import org.springframework.boot.SpringApplication;
 public class TestStoreServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(StoreServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(StoreServiceApplication::main).with(ContainersConfig.class).run(args);
     }
 }
--- store-service/src/test/java/name/snosminin/storeservice/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package name.snosminin.storeservice;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
